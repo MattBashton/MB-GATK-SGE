33ecea13f6235c49a38dd53a1c75969c4ae6cac0
Matthew Bashton <matthew.bashton@newcastle.ac.uk>
2015-10-06 10:16:11 +0100

    Added scripts for converting BAM to FASTQ
    Includes randomisation step for read order to avoid mapping bias

---
+++ Manual/bam2fastq/bam2fastq.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+#$ -cwd -V 
+#$ -pe smp 1
+#$ -l h_rt=24:00:00
+#$ -l h_vmem=6G
+#$ -R y
+
+# Matthew Bashton 2012-2015
+# Converts aligned BAM to gziped FASTQ using BAM file passed in at command-line. 
+# Uses SAMtools to randomly order reads in aligned BAM file, to avoid mapping 
+# bias as discussed here:
+# http://gatkforums.broadinstitute.org/discussion/2908/howto-revert-a-bam-file-to-fastq-format
+
+set -o pipefail
+hostname
+date
+
+source ../GATKsettings.sh
+
+BAM=$1
+B_NAME=`basename $BAM`
+
+echo "** Variables **"
+echo " - PWD = $PWD"
+echo " - BAM = $BAM"
+echo " - B_NAME = $B_NAME"
+
+echo "Copying input $BAM to $TMPDIR/"
+/usr/bin/time --verbose cp -v $BAM $TMPDIR
+
+echo "Running SAMtools on $TMPDIR/$B_NAME saving output as a gziped FASTQ file"
+/usr/bin/time --verbose $SAMTOOLS bamshuf -uOn 128 $TMPDIR/$B_NAME $TMPDIR/tmp | $SAMTOOLS bam2fq - | gzip > $TMPDIR/$B_NAME.fq.gz
+
+echo "Copying $TMPDIR/*.fq.gz to $PWD"
+/usr/bin/time --verbose cp -v $TMPDIR/*.fq.gz $PWD
+
+echo "Deleting $TMPDIR/*.bam"
+rm $TMPDIR/*.bam
+
+echo "Deleting $TMPDIR/*.gz"
+rm $TMPDIR/*.gz
+
+date
+echo "END"
