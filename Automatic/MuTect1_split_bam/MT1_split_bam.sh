7e22a9abe0a7a3624c64442bde38ac665d4ac27a
MattBasthon <matthew.bashton@newcastle.ac.uk>
2016-06-10 14:03:18 +0100

    PrintReads: reduced memory in MT1 pipeline
    Also set -nct 1 rather than -nct 2

--- Automatic/MuTect1_split_bam/MT1_split_bam.sh
+++ Automatic/MuTect1_split_bam/MT1_split_bam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 #$ -cwd -V
 #$ -pe smp 1
-#$ -l h_vmem=18G
+#$ -l h_vmem=10G
 #$ -l h_rt=24:00:00
 #$ -R y
 #$ -q all.q,bigmem.q
@@ -42,9 +42,9 @@ echo "Copying normal input $INPUT_DIR/$INPUT.merged.realigned.recalibrated.ba* t
 /usr/bin/time --verbose cp -v $INPUT_DIR/$INPUT.merged.realigned.recalibrated.bai $TMPDIR
 
 echo "Running GATK PrintReads and outputing reads from normal sample: $NORMAL"
-/usr/bin/time --verbose $JAVA -Xmx14g -jar $GATK \
+/usr/bin/time --verbose $JAVA -Xmx6g -jar $GATK \
 -T PrintReads \
--nct 2 \
+-nct 1 \
 --sample_name $NORMAL \
 -I $TMPDIR/$INPUT.merged.realigned.recalibrated.bam \
 -R $REF \
@@ -52,7 +52,7 @@ echo "Running GATK PrintReads and outputing reads from normal sample: $NORMAL"
 --log_to_file $NORMAL.PrintReads.log
 
 echo "Running GATK PrintReads and outputing reads from tumour sample: $TUMOUR"
-/usr/bin/time --verbose $JAVA -Xmx12g -jar $GATK \
+/usr/bin/time --verbose $JAVA -Xmx6g -jar $GATK \
 -T PrintReads \
 -nct 1 \
 --sample_name $TUMOUR \
