3ca6f6170b848b6e7be142d9e3e383cededc18a0
Matthew Bashton <matthew.bashton@newcastle.ac.uk>
2016-08-02 13:56:46 +0100

    Added VariantsToTable for MuTect1/2

---
+++ Automatic/VTT_MT1/VariantsToTable.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -eu
+#$ -cwd -V
+#$ -pe smp 1
+#$ -l h_rt=4:00:00
+#$ -l h_vmem=8G
+#$ -R y
+#$ -q all.q,bigmem.q
+
+# Matthew Bashton 2012-2016
+# Runs VariantsToTable to convert .vcf to .tab for MuTect1 vcf output.
+# Outputs CHROM, POS, REF, ALT, FILTER, GT:AD:DP:FA for all samples
+
+set -o pipefail
+hostname
+date
+
+source ../GATKsettings.sh
+
+# Get info for pair using task id from array job
+LINE=`awk "NR==$SGE_TASK_ID" $MUTECT_LIST`
+set $LINE
+RUN_ID=$1
+NORMAL=$2
+TUMOUR=$3
+
+# Make input name for this run
+INPUT=$NORMAL.vs.$TUMOUR
+
+#Input file path
+INPUT_DIR="../MuTect1"
+
+echo "** Variables **"
+echo " - PWD = $PWD"
+echo " - NORMAL = $NORMAL"
+echo " - TUMOUR = $TUMOUR"
+echo " - INPUT_DIR = $INPUT_DIR"
+echo " - INPUT = $INPUT"
+
+echo "Copying normal input $INPUT_DIR/$INPUT.vc* to $TMPDIR/"
+/usr/bin/time --verbose cp -v $INPUT_DIR/$INPUT.vcf $TMPDIR
+/usr/bin/time --verbose cp -v $INPUT_DIR/$INPUT.vcf.idx $TMPDIR
+
+echo "Running VariantsToTable on $INPUT"
+/usr/bin/time --verbose $JAVA -Xmx4g -jar $GATK \
+-T VariantsToTable \
+-R $REF \
+-V $TMPDIR/$INPUT.vcf \
+-F CHROM \
+-F POS \
+-F REF \
+-F ALT \
+-F FILTER \
+--showFiltered \
+-GF GT \
+-GF AD \
+-GF DP \
+-GF FA \
+-o $TMPDIR/$INPUT.tab
+
+echo "Copying $TMPDIR/$INPUT.tab to $PWD"
+/usr/bin/time --verbose cp -v $TMPDIR/$INPUT.tab $PWD
+
+echo "Deleting $TMPDIR/$INPUT.*"
+rm $TMPDIR/$INPUT.*
+
+date
+echo "END"
